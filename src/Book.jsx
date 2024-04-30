d7c805b655368807b44e540e2e4e799b9035c5ff
Krishna <varshney.kv3@gmail.com>
2024-04-30 14:46:54 +0530

    second

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,27 +1,12 @@
-import React from 'react'
-
-const Book = () => {
+const Book=(props)=>{
   return (
-    <div className='book'>
-        <Title/>
-        <Image/>
-        <Author/>
-        <Price/>
-    </div>
-    
-  )
-}
-
-const Title = () => <h1>The Power of Your Subconscious Mind</h1>;
-
-const Price = () =>{
-    return <h2>Rs.149</h2>;
-}
-const Image = () =>{
-    return <img src="https://m.media-amazon.com/images/I/61jBLw5Bq9L._SY425_.jpg"/>;
-}
-
-function Author(){
-    return <h3>Joseph Murphy</h3>;
-}
+  <div className="book">
+      <h2>{props.title}</h2>
+      <img src={props.imgLink}/>
+      <h2>{props.author}</h2>
+      <h3>{props.price}</h3>
+     
+  </div>
+  );
+};
 export default Book;
